9830e877417262999fa3c9c3cf8b849165ac2a93
zipeks <matzielinski7@gmail.com>
2023-01-20 12:28:09 +0100

    projectStart

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+import loadPage from './loadPage';
+
+loadPage();
+console.log("15")
---
+++ src/loadPage.js
@@ -0,0 +1,7 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const loadPage = () => {
+    const content = document.querySelector('#content');
+}
+export default loadPage;
